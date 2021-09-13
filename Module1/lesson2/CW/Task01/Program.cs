ca8376b4a383875f306c7eed5106b59c4742c2db
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2021-09-13 19:13:02 +0300

    Added second Classwork!

---
+++ Module1/lesson2/CW/Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        public static double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static void Main(string[] args)
+        {
+            string r = Console.ReadLine();
+            if (!double.TryParse(r,out double data)) Console.WriteLine("Неверный ввод!");
+            else Console.WriteLine($"Длина: {2*Math.PI*data:.000}\nПлощадь: {GetSquare(data):.000}");
+        }
+    }
+}
---
+++ Module1/lesson2/CW/Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void PrintStolbik(string data)
+        {
+            char[] list = data.ToCharArray();
+            for (int i = 0; i < list.Length; i++) Console.WriteLine(list[i]);
+        }
+        
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            PrintStolbik(data);
+        }
+    }
+}
