c577e63ce9a4c4a26f43ebece7f2fcbc90f37386
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2021-09-14 20:32:24 +0300

    Added third Classwork!

---
+++ Module1/lesson3/CW/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CW_3
+{
+    class Program
+    {
+
+
+        static bool Function1(bool p, bool q)
+        {
+            return !(p && q) && !(p || !q);
+        }
+
+        static void Function2(bool p, bool q, out bool ans)
+        {
+            ans = !(p && q) && !(p || !q);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("p q F");
+            for (int p = 0; p < 2; p++)
+            {
+                for (int q = 0; q < 2; q++)
+                {
+                    Console.WriteLine(
+                        $"{p} {q} {Convert.ToInt32(Function1(Convert.ToBoolean(p), Convert.ToBoolean(q)))}");
+                }
+            }
+        }
+    }
+}
---
+++ Module1/lesson3/CW/Task02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine((i+1)*(i+1));
+            }
+
+        }
+        static void Function2()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine((i+1)*(i+1));
+                i++;
+            }
+
+        }
+        static void Function3()
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine((i + 1) * (i + 1));
+                i++;
+            } while (i < 10);
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Function1();
+            Function2();
+            Function3();
+        }
+    }
+}
