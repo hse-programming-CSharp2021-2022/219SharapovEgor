cdc12b3939338e150f9d75c8a322d2921329e6ce
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2021-09-26 00:00:28 +0300

    Added 6 Homework/Classwork (6.1))

---
+++ Module1/lesson6/HW/HW-6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW_6
+{
+    class Program
+    {
+        static void Compress(int[] data)
+        {
+            int k = 0;
+            int h = 0;
+            for (int i = 0; i < data.Length-h-1; i++)
+            {
+                if ((data[i] + data[i + 1]) % 3 == 0)
+                {
+                    data[i] = data[i] * data[i + 1];
+                    h += 1;
+                    int j = 1;
+                    while (i + j < data.Length - 1)
+                    {
+                        data[i + j] = data[i + j + 1];
+                        j += 1;
+                    }
+                    k += 1;
+                }
+            }
+
+            for (int i = 0; i < data.Length - h; i++)
+            {
+                Console.Write(data[i] +" ");
+            }
+            Console.WriteLine($"\nРазмер массива сокращён на: {h}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] data = {1, 2, 3, 4, 5, 6, 7, 8};
+            Compress(data);
+        }
+
+    }
+
+}
+    
