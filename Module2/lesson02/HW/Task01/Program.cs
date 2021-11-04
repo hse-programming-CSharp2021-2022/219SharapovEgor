e11d5795e976d5e4a3e3f37fd828fba31bfc1a1d
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2021-11-04 22:29:42 +0300

    Added 14 Homework!

---
+++ Module2/lesson02/HW/Task01/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Task01
+
+{
+    class MyComplex    {
+        
+        private double _re, _im;
+        public double Real
+        {
+            get
+            {
+                return _re;
+            }
+            set
+            {
+                _re = value;
+            }
+            
+        }
+        public double Imagine
+        {
+            get
+            {
+                return _im;
+            }
+            set
+            {
+                _im = value;
+            }
+            
+        }
+
+        public MyComplex(double xre = 0f, double xim = 0f)
+        {
+            Real = xre; Imagine = xim;
+        }
+
+        public static MyComplex operator ++(MyComplex mc)
+        {
+            return new MyComplex(mc.Real++, mc.Imagine++);
+        }
+        
+        public static MyComplex operator --(MyComplex mc)
+        {
+            return new MyComplex(mc.Real-1, mc.Imagine-1);
+        }
+
+        public double Mod()
+        {
+            return Math.Abs(_re*_re+_im*_im);
+        }
+        
+        static public bool operator true(MyComplex f)  
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+        
+        static public bool operator false(MyComplex f)  
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        } 
+        
+        static public MyComplex operator +(MyComplex a, MyComplex b)  
+        {
+            return new MyComplex(a.Real+b.Real, a.Imagine + b.Imagine);
+        } 
+        
+        static public MyComplex operator -(MyComplex a, MyComplex b)  
+        {
+            return new MyComplex(a.Real-b.Real, a.Imagine - b.Imagine);
+        } 
+        
+        static public MyComplex operator *(MyComplex a, MyComplex b)  
+        {
+            return new MyComplex(a.Real*b.Real - a.Imagine - b.Imagine, a.Imagine * b.Real + a.Real*b.Imagine);
+        } 
+        
+        static public MyComplex operator /(MyComplex a, MyComplex b)  
+        {
+            return new MyComplex((a.Real*b.Real + a.Imagine*b.Imagine)/(b.Real*b.Real+b.Imagine*b.Imagine),
+                (a.Imagine*b.Real + a.Real*b.Imagine)/(b.Real*b.Real+b.Imagine*b.Imagine));
+        } 
+        
+        public override string ToString()
+        {
+            return $"{Real} + {Imagine}i";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new MyComplex(1f, 2f);
+            var c = new MyComplex(3f, 4f);
+            Console.WriteLine(q + c);
+            Console.WriteLine(q - c);
+            Console.WriteLine(q * c);
+            Console.WriteLine(q / c);
+        }
+    }
+}
---
+++ Module2/lesson02/HW/Task02/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+            public class ConsolePlate
+            {
+                char _plateChar; 
+                ConsoleColor _plateColor = ConsoleColor.White;
+                private ConsoleColor _backColor = ConsoleColor.Black;
+                
+        
+                public ConsolePlate()  
+                {
+                    _plateChar = 'A';
+                }
+                public ConsolePlate(char plateChar, ConsoleColor plateColor, ConsoleColor backColor)
+                {
+                    if (plateColor == backColor)
+                    {
+                        Console.WriteLine("Cовпадение цветов невозможно!");
+                        return;
+                    }
+                    PlateChar = plateChar;
+                    PlateColor = plateColor;
+                    BackColor = backColor;
+                }
+                public char PlateChar
+                {
+                    set
+                    {
+                        if (value >= 65 || value <= 90) 
+                            _plateChar = value;
+                        else
+                            _plateChar = 'A';
+                    }
+                    get { return _plateChar; }
+                }
+                public ConsoleColor PlateColor
+                {
+                    set
+                    {
+                        _plateColor = value;
+                    }
+                    get
+                    {
+                        return _plateColor;
+                    }
+                }
+        
+                public ConsoleColor BackColor
+                {
+                    get
+                    {
+                        return _backColor;
+                    }
+                    set
+                    {
+                        _backColor = value;
+                    }
+                }
+        
+            }
+        static void Main( )
+        {
+            ConsolePlate firstSymbol = new ConsolePlate('X', ConsoleColor.White, ConsoleColor.Red);
+            ConsolePlate secondSymbol = new ConsolePlate('O', ConsoleColor.White, ConsoleColor.Magenta);
+            int numWork;
+            string data;
+            do
+            {
+                data = Console.ReadLine();
+            }while (!int.TryParse(data, out numWork) || numWork < 2 || numWork > 35);
+            
+            for (int i = 0; i < numWork; i++)
+            {
+                for (int j = 0; j < numWork; j++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            Console.ForegroundColor = firstSymbol.PlateColor;
+                            Console.BackgroundColor = firstSymbol.BackColor;
+                            Console.Write(firstSymbol.PlateChar);
+                        }
+                        else if (j % 2 == 0)
+                        {
+                            Console.ForegroundColor = secondSymbol.PlateColor;
+                            Console.BackgroundColor = secondSymbol.BackColor;
+                            Console.Write(secondSymbol.PlateChar);
+                        }
+
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.ForegroundColor = firstSymbol.PlateColor;
+                            Console.BackgroundColor = firstSymbol.BackColor;
+                            Console.Write(firstSymbol.PlateChar);
+                        }
+                        else if (j % 2 != 0)
+                        {
+                            Console.ForegroundColor = secondSymbol.PlateColor;
+                            Console.BackgroundColor = secondSymbol.BackColor;
+                            Console.Write(secondSymbol.PlateChar);
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
