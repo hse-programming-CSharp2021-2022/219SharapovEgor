1c09f4ab5663a39feadca84cf608357ae6cc61da
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2021-11-21 14:53:15 +0300

    Added 18 Homework for 7 seminar!

---
+++ Module2/lesson07/HW/VARIANT1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace HW_18
+{
+    class Person
+    {
+        public Random rnd = new Random();
+        public string Name {set;get;}
+        public uint Age {set; get;}
+
+        public Person(string name, uint age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public virtual string Say() => "I am a simple person.";
+
+        public override string ToString()
+        {
+            return $"{Say()} My name is {Name}, my age is {Age}.";
+        }
+
+    }
+
+    class Doctor : Person
+    {
+        private uint exp;
+        public uint Exp
+        {
+            get
+            {
+                return exp;
+            }
+            set
+            {
+                if (value >= 5 && value <= 20)
+                    exp = value;
+                else
+                {
+                    exp = (uint)rnd.Next(5, 21);
+                }
+            }
+        }
+
+        public override string Say()
+        {
+            return $"I am a doctor. I've studied for {exp} years.";
+        } 
+
+        public string Heal(Person p)
+        {
+           return $"I'm gonna heal: {p}";
+        }
+
+        public Doctor(string name, uint age, uint expr) : base(name, age)
+        {
+            if (expr >= 5 && expr <= 20)
+                exp = expr;
+            else
+            {
+                exp = (uint)rnd.Next(5, 21);
+            }    
+        }
+    }
+
+    class Intern : Doctor
+    {
+        private uint exp;
+        public uint Exp
+        {
+            get => exp;
+            set
+            {
+                if (value >= 1 && value <= 4)
+                    exp = value;
+                else
+                {
+                    exp = (uint)rnd.Next(1, 5);
+                }
+            }
+        }   
+    
+        public override string Say()
+        { 
+            return $"I am an intern. I've studied for {exp} years.";
+        } 
+
+        public Intern(string name, uint age, uint expr) : base(name, age, expr)
+        {
+            if (expr >= 1 && expr <= 4)
+                exp = expr;
+            else
+            {
+                exp = (uint)rnd.Next(1, 4);
+            }       
+        }
+    }
+
+    class Program
+    {
+        static Random rnd = new Random();
+        public static string GenerateName()
+        {
+            uint n = (uint)rnd.Next(3, 11);
+            var s = new StringBuilder((int)n);
+            s.Append((char) rnd.Next('A', 'Z' + 1));
+            for (var i = 1; i < n; i++)
+                s.Append((char) rnd.Next('a', 'z' + 1));
+            return s.ToString();
+        }
+    
+        public static void Main(string[] args)
+        {
+            ConsoleKeyInfo exitKey;
+            do
+            {
+                var dr = new Doctor(GenerateName(), (uint)rnd.Next(18, 61), (uint)rnd.Next(-50, 51));
+                var intern = new Intern(GenerateName(), (uint)rnd.Next(18, 61), (uint)rnd.Next(-50, 51));
+                Console.Write("Введите число людей: ");
+                var n = uint.Parse(Console.ReadLine());
+                var persons = new Person[n];
+
+                for (var i = 0; i < n - 1; i++)
+                    persons[i] = new Person(GenerateName(), (uint)rnd.Next(120));
+            
+                persons[n - 1] = new Doctor(GenerateName(), (uint)rnd.Next(18, 61), (uint)rnd.Next(-50, 51));
+
+                for (int i = 0; i < n / 2; i++)
+                    Console.WriteLine(dr.Heal(persons[i]));
+            
+                Console.WriteLine(intern);
+            
+                for (uint i = n / 2; i < (n % 2 == 0 ? n : n - 1); i++)
+                    Console.WriteLine(dr.Heal(persons[i]));
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Для выхода нажмите Escape, чтобы повторить, нажмите любую другую кнопку!");
+                Console.ResetColor();
+                exitKey = Console.ReadKey();
+            } while (exitKey.Key != ConsoleKey.Escape);
+
+        }
+    }
+}
---
+++ Module2/lesson07/HW/VARIANT2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text;
+
+namespace VARIANT2
+{
+    class Creature
+    {
+        public static Random rnd = new Random();
+        public string Name { get;}
+        
+        public double Speed { get;}
+        
+        public Creature(string name, double speed)
+        {
+            Name = name;
+            Speed = speed;
+        }
+
+        public virtual string Run()
+        {
+            return $"I am running with a speed of {Math.Round(Speed, 1)}.";
+        }
+
+        public override string ToString()
+        {
+            return $"{Run()} My name is {Name}.";
+        }
+    }
+    class Dwarf:Creature
+    {
+        private int strenght;
+        
+        public int Strength 
+        {            
+            get
+            {
+                return strenght;
+            }
+            set
+            {
+                if (value >= 1 && value <= 20)
+                    strenght = value;
+                else
+                {
+                    strenght = rnd.Next(1, 21);
+                }
+            }
+            
+        }
+        
+        public Dwarf(string name, double speed, int strength) : base(name, speed)
+        {
+            Strength = strength;
+        }
+
+        public override string Run()
+        {
+            return $"I am running with a speed of {Math.Round(Speed, 1)}. My strength is {Strength}.";
+        }
+
+        public static void MakeNewStaff()
+        {
+            Console.WriteLine("I've created a new staff!");
+        }
+        
+        public override string ToString()
+        {
+            return $"{Run()} My name is {Name}.";
+        }
+    }
+    class Elf:Creature
+    {
+        private uint age;
+
+        private double GetElfSpeed()
+        {
+            return Math.Round(Speed + Age / 77, 1);
+        }
+        public uint Age { get; }
+
+        public Elf(string name, double speed) : base(name, speed)
+        {
+            Age = (uint) rnd.Next(100, 201);
+        }
+        
+        public override string Run()
+        {
+            return $"I am running with a speed of {GetElfSpeed()}. My age is {Age}.";
+        }
+        
+        public override string ToString()
+        {
+            return $"{Run()} My name is {Name}.";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = Creature.rnd;
+
+            string GenerateName()
+            {
+                uint n = (uint)rnd.Next(3, 11);
+                var s = new StringBuilder((int)n);
+                s.Append((char) rnd.Next('A', 'Z' + 1));
+                for (var i = 1; i < n; i++)
+                    s.Append((char) rnd.Next('a', 'z' + 1));
+                return s.ToString();
+            }
+            
+            ConsoleKeyInfo exitKey;
+            do
+            {
+                uint n;
+                do
+                {
+                    Console.Write("Введите число существ (не больше 100): ");
+                } while (!uint.TryParse(Console.ReadLine(), out n) || n > 100);
+
+                var creatures = new Creature[n];
+
+                for (var i = 0; i < creatures.Length; i++)
+                {
+                    double chance = rnd.NextDouble();
+
+                    if (chance <=  0.2)
+                    {
+                        creatures[i] = new Creature(GenerateName(), rnd.Next(10, 18) + rnd.NextDouble());
+                    }
+                    else if (chance <= 0.6)
+                    {
+                        creatures[i] = new Dwarf(GenerateName(), rnd.Next(10, 18) + rnd.NextDouble(), rnd.Next(-50, 50));
+                    }
+                    else if (chance <= 1)
+                    {
+                        creatures[i] = new Elf(GenerateName(), rnd.Next(10, 18) + rnd.NextDouble());
+                    }
+                }
+
+                for (int i = 0; i < creatures.Length; i++)
+                {
+                    if (creatures[i] is Dwarf)
+                    {
+                        Console.WriteLine(creatures[i]);
+                        Dwarf.MakeNewStaff();
+                    }
+                    else
+                    {
+                        Console.WriteLine(creatures[i]);
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Для выхода нажмите Escape, чтобы повторить, нажмите любую другую кнопку!");
+                Console.ResetColor();
+                exitKey = Console.ReadKey();
+            } while (exitKey.Key != ConsoleKey.Escape);
+        }
+    }
+}
