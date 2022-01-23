1ac153cba4039d0de61418e83ed2d9d1c59b921b
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2022-01-23 18:27:22 +0300

    Added Classwork 32!

---
+++ Module3/lesson02/CW/CW-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CW_2
+{
+    class Program
+    {
+        public delegate int Cast(double x);
+        static void Main(string[] args)
+        {
+            Cast AnonMet = delegate(double d)
+            {
+                int x = (int)Math.Round(d);
+                return (x + 1) & ~1;
+            };
+            
+            Cast LamdaExp = (double x) => ((int) x).ToString().Length;
+            
+            for (var i = 0; i < 4; i++)
+            {
+                var rnd = new Random();
+                var x = rnd.NextDouble()*1000;
+                Console.WriteLine($"x : {x}\n Anon: {AnonMet(x)}\n Lamda: {LamdaExp(x)}");
+            }
+            var rndi = new Random();
+            Cast UniCast = AnonMet;
+            UniCast += LamdaExp;
+            var  a = rndi.NextDouble()*1000; 
+            Console.WriteLine("Input: " + a);
+            Console.WriteLine("C0: " + UniCast.Invoke(a));
+            UniCast -= (double x) => ((int) x).ToString().Length;
+            Console.WriteLine($"Uni: {UniCast.Invoke(a)}");
+            UniCast -= LamdaExp;
+            Console.WriteLine($"Uni: {UniCast.Invoke(a)}");
+        }
+    }
+}
---
+++ Module3/lesson02/CW/Task02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task02
+{
+    public delegate string ConvertRule(string str);
+
+    class Program
+    {
+
+        public static string RemoveDigits(string str)
+        {
+            return Regex.Replace(str, "[0-9]", "", RegexOptions.IgnoreCase);
+        }
+
+        public static string RemoveSpaces(string str)
+        {
+            return str.Replace(" ", "");
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] data = new[] {"ssss12_21", "111    23 siera"};
+
+            ConvertRule Ans = RemoveDigits;
+            Ans += RemoveSpaces;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                foreach (ConvertRule a in Ans.GetInvocationList())
+                {
+                    Console.WriteLine(a(data[i]));
+                }
+            }
+
+        }
+    }
+}
