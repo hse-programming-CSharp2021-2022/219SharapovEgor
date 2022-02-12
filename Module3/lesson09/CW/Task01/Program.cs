926e90e26e975f3d4e68cf91af96ec72bb36fc5c
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2022-02-12 21:29:57 +0300

    Added 39 Homework and Classwork also added 310 Classwork

---
+++ Module3/lesson09/CW/Task01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task1
+{
+    using System;
+    interface IFigure
+    {
+        double Area { get; }
+    }
+    class Square : IFigure
+    {
+        double Side { get; set; }
+        public double Area { get => Math.Pow(Side, 2); }
+        public Square(double side)
+        {
+            Side = side;
+        }
+    }
+    class Circle : IFigure
+    {
+        double Radius { get; set; }
+        public double Area { get => Math.PI * Radius * Radius; }
+        public Circle(double r)
+        {
+            Radius = r;
+        }
+    }
+    class Program
+    {
+        public static void Info<T>(T[] figures, double min) where T : IFigure
+        {
+            foreach (var figure in figures)
+            {
+                if (figure.Area > min)
+                    Console.WriteLine($"Type: {figure.GetType()}\nArea:{figure.Area}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            IFigure[] figures = new IFigure[10];
+            Circle[] circles = new Circle[10];
+            Square[] squares = new Square[10];
+            Random rnd = new();
+            for (int i = 0; i < 10; i++)
+            {
+                if (rnd.Next(2) == 1)
+                    figures[i] = new Circle(rnd.Next(11));
+                else
+                    figures[i] = new Square(rnd.Next(11));
+                squares[i] = new Square(rnd.Next(11));
+                circles[i] = new Circle(rnd.Next(11));
+            }
+            Info<IFigure>(figures, 5);
+            Console.WriteLine("***");
+            Info<Circle>(circles, 5);
+            Console.WriteLine("***");
+            Info<Square>(squares, 5);
+        }
+    }
+}
---
+++ Module3/lesson10/CW/Task01/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+class Stack<T>
+{
+    T value;
+    Stack<T> pred;
+    public int Size { get; set; }
+    public bool IsEmpty { get; set; }
+    public Stack(T value, Stack<T> pred)
+    {
+        this.value = value;
+        this.pred = pred;
+        IsEmpty = false;
+        if(pred != null)
+            Size = pred.Size + 1;
+        else
+            Size = 1;
+    }
+    public Stack()
+    {
+        IsEmpty = true;
+    }
+    public Stack<T> Push(T newValue)
+    {
+        return new Stack<T>(newValue, this);
+    }
+    public T Pop()
+    {
+        if (IsEmpty)
+        {
+            throw new ArgumentException();
+        }
+        T ans = value;
+        value = pred.value;
+        if (pred != null)
+            pred = pred.pred;
+        else
+            IsEmpty = false;
+        return ans;
+    }
+    public T Top()
+    {
+        return value;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var data = new Stack<int>();
+        
+        data.Push(10);
+        data.Push(9);
+        data.Push(8);
+        
+        Console.WriteLine(data.Top());
+        Console.WriteLine(data.Pop());
+        Console.WriteLine(data.Top());
+    }
+}
